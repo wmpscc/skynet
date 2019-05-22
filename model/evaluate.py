35cbf0faf4e9625a435b49a754ac3efa6041bd2e
superhard <liqin@mafengwo.com>
2019-05-22 11:12:22 +0800

    数据库操作

---
+++ model/evaluate.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from model.connection import getDBSession
+import json
+
+Base = declarative_base()
+
+class Evaluate(Base):
+    # 表的名字
+    __tablename__ = 'evaluate'
+
+    # 表的结构
+    unique_id = Column(String(32), primary_key=True)
+    bayes_model = Column(String(20))
+    knn_model = Column(String(20))
+    tree_model = Column(String(20))
+
+class DbEvaluate:
+
+    def __init__(self):
+
+        self.session = getDBSession()
+
+    def _getModel(self, unique_id):
+        return self.session.query(Evaluate).filter_by(unique_id=unique_id).first()
+
+    def getKnnModel(self, unique_id):
+        my_evaluate = self._getModel(unique_id)
+
+        return json.loads(my_evaluate.knn_model)
+
+
