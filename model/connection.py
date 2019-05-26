43a2e2ddaddb5d1f7931096e7c4e0a2b030b62a3
superhardqin <526860071@qq.com>
2019-05-26 11:17:30 +0800

    fix bug

--- model/connection.py
+++ model/connection.py
@@ -2,9 +2,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+dbname = "car"
 # 数据库连接
 def getDBSession():
-    engine = create_engine('mysql+mysqlconnector://root:421421mfw@localhost:3306/car')
+    engine = create_engine('mysql+mysqlconnector://root:421421mfw@localhost:3306/'+dbname)
 
     # 创建DBSession类型
     DBSession = sessionmaker(bind=engine)
