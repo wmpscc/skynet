88ce9df0026eca85104e2ed7cd39c3c7dd2c8489
superhardqin <526860071@qq.com>
2019-05-29 10:56:23 +0800

    增加处理方法

--- service/train.py
+++ service/train.py
@@ -1,5 +1,8 @@
 # -*- coding: UTF-8 -*-
 import math
+import numpy as np
+from sklearn import preprocessing
+
 def getModelByGroup(group, type, trainModel, groupNum, testPercentage):
     group = int(group)
     feature = trainModel['feature']
@@ -35,3 +38,44 @@ def getModelByGroup(group, type, trainModel, groupNum, testPercentage):
         resultClassificate = needClassificate[:testEnd]
 
     return resultFeature, resultClassificate
+
+# z-score归一化
+def z_score_norm(data_set):
+    return preprocessing.MinMaxScaler().fit_transform(data_set)
+
+def coverFloat(master):
+    master_float = []
+    for i in master:
+        i = float(i)
+        master_float.append(i)
+    return master_float
+
+def similarSort(master, origin):
+    #转化
+
+    master = coverFloat(master)
+
+    list = origin
+    list.append(master)
+    list = np.array(list)
+    list = z_score_norm(list)
+
+    #归一化取出
+    total = list.shape[0]
+    master = list[total-1]
+    list = np.delete(list, total-1, 0)
+
+    #列表长度
+    list_size = list.shape[0]
+
+    diff_mat = np.tile(master, (list_size, 1)) - list
+    sq_diff_mat = diff_mat ** 2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances ** 0.5
+    sorted_distIndicies = distances.argsort()
+
+    result_list = []
+    for i in range(list_size):
+        result_list.append(origin[sorted_distIndicies[i]])
+
+    return result_list
