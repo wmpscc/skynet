81a7329e4006d7f10e446d818e5ffa1abdd17519
superhardqin <526860071@qq.com>
2019-05-30 21:56:07 +0800

    增加相似处理

--- model/evaluate.py
+++ model/evaluate.py
@@ -42,3 +42,20 @@ class DbEvaluate:
         origin = json.loads(my_evaluate.tree_model)
         return origin["train"]
 
+    def getKnn(self, unique_id):
+        my_evaluate = self._getModel(unique_id)
+        origin = json.loads(my_evaluate.knn_model)
+        return origin
+
+    def getBayes(self, unique_id):
+        my_evaluate = self._getModel(unique_id)
+
+        origin = json.loads(my_evaluate.bayes_model)
+        return origin
+
+    def getTree(self, unique_id):
+        my_evaluate = self._getModel(unique_id)
+
+        origin = json.loads(my_evaluate.tree_model)
+        return origin
+
