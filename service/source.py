20d70bd7a345df5b7b37e214702e511a76f20b6d
superhardqin <526860071@qq.com>
2019-06-14 22:05:27 +0800

    增加相似处理

---
+++ service/source.py
@@ -0,0 +1,88 @@
+# -*- coding: UTF-8 -*-
+import sys
+import fasttext
+import jieba
+import random
+import math
+
+sys.path.append('..')
+
+from model.source import DbSource
+
+# 写入训练的原始数据（待分词）
+def writeTrainTxt():
+    db = DbSource()
+    id = 0
+    out = open("../resources/content/source_origin.txt", 'w')
+
+    num = 0
+    sell_success = 0
+    buy_success = 0
+    default_success = 0
+
+    while(True):
+        lines = db.getDetermineForeachList(id, 9)
+        if (len(lines) > 0) :
+            for line in lines:
+                id = line.id
+                text = (line.title+line.content).strip()
+                if (len(text) > 0):
+
+                    if (line.intention == 2):
+                        # 普通帖子
+                        out.write(text+" __label__default\n")
+
+                        num+=1
+                        default_success+=1
+                    elif (line.intention == 5):
+                        # 购买意向帖子
+                        out.write(text + " __label__buy\n")
+
+                        num += 1
+                        buy_success+=1
+                    elif (line.intention == 9):
+                        # 出售意向帖子
+                        out.write(text + " __label__sell\n")
+
+                        num += 1
+                        sell_success+=1
+        else:
+            break
+    out.close()
+
+    print ("total:%d" % num)
+    print ("sell:%d" % sell_success)
+    print ("buy:%d" % buy_success)
+    print ("default:%d" % default_success)
+
+def trainModel():
+    # 写入训练原始数据
+    writeTrainTxt()
+
+    lines = _get_line("../resources/content/source_origin.txt")
+
+    source_file = "../resources/content/source.txt"
+    out = open(source_file, 'w')
+    for li in lines:
+        out.write(li)
+
+    classifier = fasttext.supervised(source_file, '../resources/model/source_online', label_prefix='__label__', epoch=50)
+    result = classifier.test(source_file)
+    print ("\n")
+    print ('P@1:', result.precision)
+    print ('R@1:', result.recall)
+    print ('Number of examples:', result.nexamples)
+
+def _get_line(file):
+    fr = open(file)
+    need = []
+    tag = " __label__"
+    for line in fr.readlines():
+        line = line.strip()
+        if(len(line) >0 ):
+            struct = line.split(tag)
+
+            content = " ".join(jieba.lcut(struct[0]))
+            need.append(content+tag+struct[1]+"\n")
+    random.shuffle(need)
+    return need
