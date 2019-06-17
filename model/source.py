a33555e174002814147569a44e2ba5d1bfb1fed7
superhardqin <526860071@qq.com>
2019-06-17 17:03:51 +0800

    测试模型

--- model/source.py
+++ model/source.py
@@ -3,6 +3,8 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from model.connection import getDBSession
 
+import model.generator
+
 Base = declarative_base()
 
 class Source(Base):
@@ -17,6 +19,7 @@ class Source(Base):
     content = Column(String)
     status = Column(Integer)
     intention = Column(Integer)
+    operate_sort = Column(Integer)
 
 
 class DbSource:
@@ -31,22 +34,35 @@ class DbSource:
     def getDetermineForeachList(self, id, status):
         return self.session.query(Source).filter(Source.id >id, Source.status==status).order_by(Source.id.asc()).limit(100).all()
     def suspectedSell(self, id):
+
+        db = model.generator.DbGenerator()
+        operate_sort = db.id()
+
         cur = self.session.query(Source).get(id)
         cur.intention = 8
         cur.status = 9
+        cur.operate_sort = operate_sort
         self.session.add(cur)
         self.session.commit()
 
     def suspectedBuy(self, id):
+        db = model.generator.DbGenerator()
+        operate_sort = db.id()
+
         cur = self.session.query(Source).get(id)
         cur.intention = 4
         cur.status = 9
+        cur.operate_sort = operate_sort
         self.session.add(cur)
         self.session.commit()
 
     def suspectedDefault(self, id):
+        db = model.generator.DbGenerator()
+        operate_sort = db.id()
+
         cur = self.session.query(Source).get(id)
         cur.intention = 1
         cur.status = 9
+        cur.operate_sort = operate_sort
         self.session.add(cur)
         self.session.commit()
